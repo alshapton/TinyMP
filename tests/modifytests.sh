b0dd0cc69791510da732a70e1773f371e9eb2599
alshapton <alshapton@gmail.com>
2017-11-13 20:01:45 +0000

    Rewritten storage class
    Also added checkout of latest tinydb tests and modified them to work with the MsgPackStorage class "on demand"

---
+++ tests/modifytests.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "import msgpack" >> conftest.py
+echo "import tinymp" >> conftest.py
+
+sed -i -e '/import/!s/JSONStorage/MsgPackStorage/g' *.py
+sed -i -e '/import/!s/JSON/MsgPack/g' *.py
+sed -i -e '/import/!s/json/MsgPack/g' *.py
+
+echo "import msgpack" >> test_storages.py
+echo "from tinymp import *" >> test_storages.py
+
+echo "import msgpack" >> test_middlewares.py
+echo "from tinymp import *" >> test_middlewares.py
--- tinymp.py
+++ tinymp.py
@@ -2,26 +2,46 @@
 import msgpack
 from tinydb import Storage
 
+import os
+
+def touch(fname, create_dirs):
+    if create_dirs:
+        base_dir = os.path.dirname(fname)
+        if not os.path.exists(base_dir):
+            os.makedirs(base_dir)
+
+    with open(fname, 'a'):
+        os.utime(fname, None)
+
 class MsgPackStorage(Storage):
-    def __init__(self, filename):  
-        self.filename = filename
+    def __init__(self, path, create_dirs=False, **kwargs):
+        #self.filename = filename
+        super(MsgPackStorage, self).__init__()
+        touch(path, create_dirs=create_dirs)  # Create file if not exists
+        self.kwargs = kwargs
+        self._handle = open(path, 'r+')
 
-    def read(self):
-        try:
-            open(self.filename)
-        except IOError:
-            return None 
-
-        with open(self.filename) as handle:
-            try:
-                data = msgpack.unpackb(handle.read()) 
-                return data
-            except IOError:
-                return None 
 
     def write(self, data):
-        with open(self.filename, 'w') as handle:
-            msgpack.dump(data, handle)
+        self._handle.seek(0)
+        serialized = msgpack.dump(data, self._handle)
+        #self._handle.write(serialized)
+        self._handle.flush()
+        self._handle.truncate()
 
-    def close(self): 
-        pass
+
+    def read(self):
+        # Get the file size
+        self._handle.seek(0, os.SEEK_END)
+        size = self._handle.tell()
+
+        if not size:
+            # File is empty
+            return None
+        else:
+            self._handle.seek(0)
+            return  msgpack.unpackb(self._handle.read()) 
+
+
+    def close(self):
+        self._handle.close()
