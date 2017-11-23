f0636a9b95c55c5f755f46ec7ce30c1681f2c5b0
alshapton <alshapton@gmail.com>
2017-11-23 14:08:15 +0000

    Update modifytests.sh

--- tests/modifytests.sh
+++ tests/modifytests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "import msgpack" >> conftest.py
-echo "import tinymp" >> conftest.py
+echo "import .tinymp" >> conftest.py
 
 sed -i -e '/import/!s/JSONStorage/MsgPackStorage/g' *.py
 sed -i -e '/import/!s/JSON/MsgPack/g' *.py
@@ -9,7 +9,7 @@ sed -i -e '/import/!s/json/MsgPack/g' *.py
 
 
 echo "import msgpack" >> test_storages.py
-echo "from tinymp import *" >> test_storages.py
+echo "from .tinymp import *" >> test_storages.py
 
 echo "import msgpack" >> test_middlewares.py
-echo "from tinymp import *" >> test_middlewares.py
+echo "from .tinymp import *" >> test_middlewares.py
