3c65092a0a85850897beaa88495cce08e982b624
alshapton <alshapton@gmail.com>
2017-12-18 12:15:32 +0000

    Update modifytests.sh

--- tests/modifytests.sh
+++ tests/modifytests.sh
@@ -9,10 +9,6 @@
 # Create new copies of the Python scripts for each StorageClass subtype
 #ls -1  test*.py| awk 'BEGIN     { FS = "." } { print "cp " $0 " " $1".msgpack.py; cp "$0 " " $1 ".umsgpack.py"  }' 
 
-# Add the new Python 3 compatiblity line to files....
-sed -i -e "1s/^/from __future__ import absolute_import\n/" conftest.py
-sed -i -e "1s/^/from __future__ import absolute_import\n/" test_storages.py
-sed -i -e "1s/^/from __future__ import absolute_import\n/" test_middlewares.py
 
 echo "import umsgpack as msgpack" >> conftest.py
 echo "import tinymp" >> conftest.py
@@ -23,7 +19,7 @@ sed -i -e '/import/!s/json/MsgPack/g' *.py
 
 
 echo "import umsgpack as msgpack" >> test_storages.py
-echo "from .tinymp import *" >> test_storages.py
+echo "from tinymp import *" >> test_storages.py
 
 echo "import umsgpack as msgpack" >> test_middlewares.py
-echo "from .tinymp import *" >> test_middlewares.py
+echo "from tinymp import *" >> test_middlewares.py
