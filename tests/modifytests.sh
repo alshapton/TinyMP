fe1fdb022a1b6bc363434fba3d0c062c9119c97a
alshapton <alshapton@gmail.com>
2017-12-20 21:06:48 +0000

    Added future import

--- tests/modifytests.sh
+++ tests/modifytests.sh
@@ -9,7 +9,10 @@
 # Create new copies of the Python scripts for each StorageClass subtype
 #ls -1  test*.py| awk 'BEGIN     { FS = "." } { print "cp " $0 " " $1".msgpack.py; cp "$0 " " $1 ".umsgpack.py"  }' 
 
+# from future import absolute_import
 
+
+echo "from future import absolute_import" >> conftest.py
 echo "import umsgpack as msgpack" >> conftest.py
 echo "import tinymp" >> conftest.py
 
@@ -18,8 +21,10 @@ sed -i -e '/import/!s/JSON/MsgPack/g' *.py
 sed -i -e '/import/!s/json/MsgPack/g' *.py
 
 
+echo "from future import absolute_import" >> test_storages.py
 echo "import umsgpack as msgpack" >> test_storages.py
 echo "from tinymp import *" >> test_storages.py
 
+echo "from future import absolute_import" >> test_middlewares.py
 echo "import umsgpack as msgpack" >> test_middlewares.py
 echo "from tinymp import *" >> test_middlewares.py
